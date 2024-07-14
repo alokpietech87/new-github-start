a506e57ff42621cfb257e21f9c5aa5e30ebf4e2c
alokpietech87 <alok@pietechsolution.com>
2024-07-14 22:57:30 +0530

    alok2 ka 1st commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ alok=10;
 
 let arr=['asmdkdjksksd', 'snsnnsa'];
 
-
 let a="nya  branch bna diya bhai lo alok name se";
 let bsd="kya bhai 2nd commit kr diye 2nd branch me";
 
 let alok='new branch bna diya alok1 naam se';
+
+let alok2='alok2 naam ka nya branch bna diya'
